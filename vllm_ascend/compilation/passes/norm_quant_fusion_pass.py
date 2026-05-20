1787295322782cd15e39c230dab319b7313e52dc
yangsiqt <duzhaoyang123@qq.com>
2026-05-20 12:57:58 +0000

    fix: support ernie mtp graph on ascend 910b

--- vllm_ascend/compilation/passes/norm_quant_fusion_pass.py
+++ vllm_ascend/compilation/passes/norm_quant_fusion_pass.py
@@ -23,7 +23,7 @@ from vllm.config.compilation import Range
 from vllm.logger import logger
 
 from vllm_ascend.compilation.passes.base_pattern import BasePattern
-from vllm_ascend.utils import enable_custom_op
+from vllm_ascend.utils import enable_add_rms_norm_bias_custom_op
 
 
 class AddRMSNormQuantPattern(BasePattern):
@@ -492,7 +492,7 @@ class AddRMSNormQuantFusionPass(VllmInductorPass):
         for eps in common_epsilons:
             AddRMSNormDynamicQuantPattern(vllm_config, eps=eps).register(self.pattern_match_passes)
             AddRMSNormDynamicQuantSPPattern(vllm_config, eps=eps).register(self.pattern_match_passes)
-            if enable_custom_op():
+            if enable_add_rms_norm_bias_custom_op():
                 AddRMSNormQuantPattern(vllm_config, eps=eps).register(self.pattern_match_passes)
                 AddRMSNormQuantSPPattern(vllm_config, eps=eps).register(self.pattern_match_passes)
                 AddRMSNormQuantPatternWithBias(vllm_config, eps=eps).register(self.pattern_match_passes)
